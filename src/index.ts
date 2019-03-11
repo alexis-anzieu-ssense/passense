5e7de2c6f80279c329ccef2a064ff6d694fada09
Alexis ANZIEU <alexis.anzieu@gmail.com>
2019-03-10 22:43:38 -0400

    update doc and coverage

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import TypeValidator from './validators/TypeValidator'
 
 export function validatePassword(password: any) {
     return TypeValidator.isValid(password) && LengthValidator.isValid(password);
-};
+}
